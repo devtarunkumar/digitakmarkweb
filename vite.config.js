fcfd4124c32ffd6b34ffe454224a6cc943ec1d74
unknown <taruncmpt07@gmail.com>
2026-04-04 19:19:20 +0530

    fix vite base path

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/",
   optimizeDeps: {
     include: ['gsap', '@gsap/react'],
     force: true // Force dependency pre-bundling
